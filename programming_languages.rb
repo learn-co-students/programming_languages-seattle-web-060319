85cfd56f51273c7e27b4b69484f32da993dde3ec
github username <github email address>
2019-05-29 18:35:54 -0700

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  	# your code here
+	h = Hash.new
+  	languages.each do |style, lang_hash|
+  		lang_hash.each do |lang_name, type_hash|
+  			if h.has_key?(lang_name) == false
+  				#builds a new key/value in the hash
+				h[lang_name] = {:type => type_hash[:type],
+					:style => [style]}
+			else
+				h[lang_name][:style] << style
+			end
+		end
+	end
+	
+	return h
+
 end
